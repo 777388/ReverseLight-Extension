8ba23be5ffbe956e630e40ffc454a0451ffd8354
777388 <96343159+777388@users.noreply.github.com>
2023-08-21 17:28:30 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.contextMenus.create({
-  id: "Reverse Light",
+  id: "reverselight",
   title: "Reverse Light",
   contexts: ["selection"]
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (info.menuItemId === "openArchive") {
+  if (info.menuItemId === "reverselight") {
     const selectedText = info.selectionText;
     for (let i = 1; i <= 255; i += 10){
         const archiveUrl = `https://www.google.com/search?q=%28site:***${i} %7C site:***${i+1} %7C site:***${i+2} %7C site:***${i+3} %7C site:***${i+4} %7C site:***${i+5} %7C site:***${i+6} %7C site:***${i+7} %7C site:***${i+8} %7C site:***${i+9}%29 %26 ${selectedText}`
